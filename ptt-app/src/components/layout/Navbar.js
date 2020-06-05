fdc5c631a76f677381a2850174f18a3856962939
Monica Silva <msilva78617@gmail.com>
2020-06-04 21:01:23 -0500

    navbar before fixing the routes

--- ptt-app/src/components/layout/Navbar.js
+++ ptt-app/src/components/layout/Navbar.js
@@ -5,39 +5,24 @@ import banner from '../../assets/banner-img.png'
 class Navbar extends Component {
         render(){
         return (
-            <div className="navbar">
-                <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-            <a className="navbar-brand" href="#">Navbar</a>
-            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                <span className="navbar-toggler-icon"></span>
-            </button>
-    ​
-            <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul className="navbar-nav mr-auto">
-                <li className="nav-item active">
-                    <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
-                </li>
-                <li className="nav-item">
-                    <a className="nav-link" href="#">Link</a>
-                </li>
-                <li className="nav-item dropdown">
-                    <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                    Dropdown
-                    </a>
-                    <div className="dropdown-menu" aria-labelledby="navbarDropdown">
-                    <a className="dropdown-item" href="#">Action</a>
-                    <a className="dropdown-item" href="#">Another action</a>
-                    <div className="dropdown-divider"></div>
-                    <a className="dropdown-item" href="#">Something else here</a>
-                    </div>
-                </li>
-                <li className="nav-item">
-                    <a className="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
-                </li>
-                </ul>
-                </div>
+            <div className="navbarComponent" width="100%">
+                <nav className="navbar navbar-dark bg-dark">
+                    <ul class="nav">
+                        <li class="nav-item">
+                            <a class="nav-link" href="https://www.bastropfoodpantry.org/">BASTROP COUNTY FOOD PANTRY</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="/">RECIPES</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="https://www.bastropfoodpantry.org/contact/">CONTACT US</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="/login" tabindex="-1" aria-disabled="true">LOGIN</a>
+                        </li>
+                    </ul>
                 </nav>
-                <img src={banner} alt="Bastrop Country Food Pantry Banner" />
+                <img src={banner} alt="Bastrop County Food Pantry Banner" width="100%" height="15%"/>
             </div>
 
             )    
