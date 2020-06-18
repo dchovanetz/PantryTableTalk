dcde9d1fc5006ada03c03c9f82f59470ea29a840
Monica Silva <msilva78617@gmail.com>
2020-06-18 12:00:05 -0500

    trying to figure out routes for shoopinglist anf recipe results

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,10 @@ import React from 'react';
 import './App.css';
 import Navbar from './components/layout/Navbar';
 import Footer from './components/layout/Footer';
+import { Router, Route, Link } from 'react-router'
+import RecipeResult from './components/pages/recipe-result/RecipeResult';
+import ShoppingList from './components/pages/shopping-list/ShoppingList';
+
 
 
 
@@ -10,11 +14,15 @@ import Footer from './components/layout/Footer';
 
 function App () {
     return (
+  <Router>
     <div>
       <Navbar />
+     
       <Footer />
     </div>
-  
+    <Route path="/reciperesult" component= { RecipeResult } />
+    <Route path="/shoppinglist" component= { ShoppingList } />
+  </Router>
   )
 }
 
--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React from 'react';
 import banner from '../../assets/banner-img.png';
 import logo from '../../assets/pantry-logo.png'
@@ -35,56 +34,3 @@ function Navbar () {
 
 
 export default Navbar
-=======
-import React, { Component } from "react";
-import banner from "../../assets/banner-img.png";
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
-import RecipeApp from "../pages/recipe-homepage/RecipeApp";
-// import LoginApp from '../pages/login-page/LoginApp';
-
-function Navbar() {
-  return (
-    <Router>
-      <div className="navbarComponent" width="100%">
-        <nav className="navbar navbar-dark bg-dark">
-          <ul className="nav">
-            <li className="nav-item">
-              <a className="nav-link" href="https://www.bastropfoodpantry.org/">
-                HOME
-              </a>
-            </li>
-            <li className="nav-item">
-              <Link className="nav-link" to="/recipe">
-                RECIPES
-              </Link>
-            </li>
-            <li className="nav-item">
-              <a
-                className="nav-link"
-                href="https://www.bastropfoodpantry.org/contact/"
-              >
-                CONTACT & LOCATION
-              </a>
-            </li>
-            <li className="nav-item">
-              <Link className="nav-link" to="/login">
-                LOGIN
-              </Link>
-            </li>
-          </ul>
-        </nav>
-        <img
-          src={banner}
-          alt="Bastrop County Food Pantry Banner"
-          width="100%"
-          height="15%"
-        />
-      </div>
-      <Route path="/recipe" component={RecipeApp} />
-      {/* <Route path="/login" component= { LoginApp } /> */}
-    </Router>
-  );
-}
-
-export default Navbar;
->>>>>>> jesus_recipesBE
