288c1f3ada25ae4aa31055c4a2a9ad2f851780d1
DavidChovanetz <dchovanetz@gmail.com>
2020-07-05 14:04:17 -0500

    recipeCard

--- client/src/components/pages/recipe-homepage/RecipeCard.js
+++ client/src/components/pages/recipe-homepage/RecipeCard.js
@@ -1,19 +1,20 @@
 import React, { Component } from 'react';
 import Card from "react-bootstrap/Card";
-import CardDeck from "react-bootstrap/CardDeck";
+// import CardDeck from "react-bootstrap/CardDeck";
+import CardGroup from "react-bootstrap/CardGroup";
 import './RecipeCard.css'
 import macaroni from '../../../assets/macaroni_cheese2.jpg'
-import chicken_tortilla from '../../../assets/chicken_tortilla.jpg'
-import kimchi from '../../../assets/kimchi_grilled_cheese.jpg'
-import fish from '../../../assets/grilled_fish_tacos.jpg'
-// import chili from '../../../assets/chili.jpg'
-import caesar from '../../../assets/caesar_salad.jpg'
+// import chicken_tortilla from '../../../assets/chicken_tortilla.jpg'
+// import kimchi from '../../../assets/kimchi_grilled_cheese.jpg'
+// import fish from '../../../assets/grilled_fish_tacos.jpg'
+// // import chili from '../../../assets/chili.jpg'
+// import caesar from '../../../assets/caesar_salad.jpg'
 
 //Title, img
 class RecipeCard extends Component {    
     render() {
         return(
-<div className='card-deck'>
+/*{ <div className='card-deck'>
 <CardDeck  className='card-deck' style={{ width: '90rem',}}>
 
   <Card className='test' style={{margin:'2rem'}}>
@@ -44,28 +45,69 @@ class RecipeCard extends Component {
     <Card.Body className = 'link'>
     <Card.Link className='link' href="/recipes/recipe-result">Fish Tacos Recipe</Card.Link>
     </Card.Body>
-  </Card>
+  </Card> }*/
 
-  {/* <Card className='test1' style={{margin:'2rem'}}>
+  /* {<Card className='test1' style={{margin:'2rem'}}>
   <Card.Img className = 'macaroni' variant="top" src={chili} />
     <Card.Body className = 'link'>
     <Card.Link className='link' href="/recipes/recipe-result">Chili Recipe</Card.Link>
     </Card.Body>
-  </Card> */}
+  </Card> }*/
 
-  <Card style={{margin:'2rem'}}>
-  <Card.Img className = 'macaroni' variant="top" src={caesar} />
-    <Card.Body className = 'link'>
-    <Card.Link className='link' href="/recipes/recipe-result">Grilled Chicken Caesar Salad Recipe</Card.Link>
+//   <Card style={{margin:'2rem'}}>
+//   <Card.Img className = 'macaroni' variant="top" src={caesar} />
+//     <Card.Body className = 'link'>
+//     <Card.Link className='link' href="/recipes/recipe-result">Grilled Chicken Caesar Salad Recipe</Card.Link>
+//     </Card.Body>
+//   </Card>
+// </CardDeck>
+
+// </div>
+<div>
+<CardGroup>
+  <Card>
+    <Card.Img variant="top" src={macaroni} />
+    <Card.Body>
+      <Card.Title>Card title</Card.Title>
+      <Card.Text>
+        This is a wider card with supporting text below as a natural lead-in to
+        additional content. This content is a little bit longer.
+      </Card.Text>
     </Card.Body>
+    <Card.Footer>
+      <small className="text-muted">Last updated 3 mins ago</small>
+    </Card.Footer>
   </Card>
-</CardDeck>
+  {/* <Card>
+    <Card.Img variant="top" src="holder.js/100px160" />
+    <Card.Body>
+      <Card.Title>Card title</Card.Title>
+      <Card.Text>
+        This card has supporting text below as a natural lead-in to additional
+        content.{' '}
+      </Card.Text>
+    </Card.Body>
+    <Card.Footer>
+      <small className="text-muted">Last updated 3 mins ago</small>
+    </Card.Footer>
+  </Card>
+  <Card>
+    <Card.Img variant="top" src="holder.js/100px160" />
+    <Card.Body>
+      <Card.Title>Card title</Card.Title>
+      <Card.Text>
+        This is a wider card with supporting text below as a natural lead-in to
+        additional content. This card has even longer content than the first to
+        show that equal height action.
+      </Card.Text>
+    </Card.Body>
+    <Card.Footer>
+      <small className="text-muted">Last updated 3 mins ago</small>
+    </Card.Footer>
+  </Card> */}
+</CardGroup>
 
 </div>
-
-
-
-
         )
     }
 }
