0eede41959edc5779e6e000ad94b1339249c0da2
Carol Campbell <ccamp08@sbcglobal.net>
2020-07-06 03:30:39 -0500

    db connection fix

--- server.js
+++ server.js
@@ -13,23 +13,20 @@ const items = require("./routes/api/itemsRoutes");
 const recipes = require("./routes/api/recipeRoutes");
 
 const db_name = process.env.DB_NAME
-const pwd = process.env.PWD
+const db_pwd = process.env.DB_PWD
 const port = process.env.PORT || 5000;
-
-console.log(`-------db_name:  ${db_name}`);
-console.log(`-------pwd:  ${pwd}`);
 
 //Body Parser middleware
 app.use(bodyParser.json());
 
 app.get("/test", (req, res) => {
-//recipeScraper("https://www.centraltexasfoodbank.org/recipe/cheesy-macaroni-bake")
-//recipeScraper("https://www.centraltexasfoodbank.org/recipe/strawberry-basil-infused-water")
+///recipeScraper("https://www.centraltexasfoodbank.org/recipe/cheesy-macaroni-bake")
+recipeScraper("https://www.centraltexasfoodbank.org/recipe/strawberry-basil-infused-water")
 // recipeScraper("https://www.centraltexasfoodbank.org/recipe/chicken-lettuce-wraps")
-//recipeScraper("https://www.centraltexasfoodbank.org/recipe/zucchini-noodles")
-//recipeScraper("https://www.centraltexasfoodbank.org/recipe/peanut-butter-hummus")
-//recipeScraper("https://www.centraltexasfoodbank.org/recipe/overnight-oats")
-recipeScraper("https://www.centraltexasfoodbank.org/recipe/grilled-cheese-mushroom-spinach-and-tomato") //
+// recipeScraper("https://www.centraltexasfoodbank.org/recipe/zucchini-noodles")
+// recipeScraper("https://www.centraltexasfoodbank.org/recipe/peanut-butter-hummus")
+// recipeScraper("https://www.centraltexasfoodbank.org/recipe/overnight-oats")
+// recipeScraper("https://www.centraltexasfoodbank.org/recipe/grilled-cheese-mushroom-spinach-and-tomato") //
   .then((recipe) => {
     res.send(recipe)
     console.log(JSON.stringify(recipe));
@@ -39,21 +36,16 @@ recipeScraper("https://www.centraltexasfoodbank.org/recipe/grilled-cheese-mushro
     })
   });
 
-    /*
+/* The path to access will be:
 "mongodb+srv://ACCBootcampRecipe:<password>@cluster0-6o8hs.mongodb.net/<dbname>?retryWrites=true&w=majority"
-*/
-    //The path to access will be:
-    /* `mongodb+srv://ACCBootcampRecipe:${pwd}@cluster0-6o8hs.mongodb.net/${db_name}?retryWrites=true&w=majority`
 
-DB_NAME and PWD are in .env file. npm install dotenv
-*/
-//let mongoUrl = 'mongodb+srv://ACCBootcampRecipe:ACCrocksMS@cluster0-6o8hs.mongodb.net/Pantry_Talk?retryWrites=true&w=majority'
+DB_NAME and DB_PWD are in .env file. */
 
-let mongoUrl = `mongodb+srv://ACCBootcampRecipe:${pwd}@cluster0-6o8hs.mongodb.net/${db_name}?retryWrites=true&w=majority`
+let mongoUrl = `mongodb+srv://ACCBootcampRecipe:${db_pwd}@cluster0-6o8hs.mongodb.net/${db_name}?retryWrites=true&w=majority`
 
-
+//console.log('----mongoUrl: ' + mongoUrl) 
 mongoose
-  //.connect("mongodb://localhost:27017/Pantry_Talk", {
+  //.connect("mongodb://localhost:27017/Pantry_Talk", { 
   .connect(mongoUrl, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
