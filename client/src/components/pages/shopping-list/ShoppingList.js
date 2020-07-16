c3dec5313f003b46cd68ba214d1faa8017ee2dc0
Monica Silva <msilva78617@gmail.com>
2020-07-15 22:04:40 -0500

    playing with code

--- client/src/components/pages/shopping-list/ShoppingList.js
+++ client/src/components/pages/shopping-list/ShoppingList.js
@@ -3,16 +3,49 @@ import Ingredients from '../recipe-result/ingredients'
 
 function ShoppingList (props) {
 
-
+    const [items, setItems] = useState([
+      "oranges", "apples", "candy"
+    ]);
     return (
-
-    <div id='list-container'>
-        <h1>This is the shopping list</h1>
-      < Ingredients />
-    </div>
-
+      <div id="list-container">
+        <ListDisplay items={items}/>
+        <InputText handleSubmit={(item) => {
+            setItems(items.concat(item));
+          }}
+        />
+      </div>
+    )
+  }
+  const ListItem = (props) => (
+    <li>{props.name}</li>
   )
-}
+  const ListDisplay = (props) => {
+    const items = props.items.map((item, i) => (
+      <ListItem
+        key={i}
+        name={item}
+      />
+    ))
+    return (
+      <ul>
+        {items}
+      </ul>
+    )
+  }
+  const InputText = (props) => {
+    const [value, setValue] = useState('');
+    return (
+      <form onSubmit={(e) => {
+        e.preventDefault();
+        props.handleSubmit(value);
+        setValue('');
+      }}>
+      <input type="text" value={value} onChange={e => setValue(e.target.value)}/>
+      </form>
+    )
+  }
+
+  
 
 
 export default ShoppingList
