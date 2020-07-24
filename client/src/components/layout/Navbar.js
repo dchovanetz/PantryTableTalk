7004519838b238c14c93a808b204041700d00418
jaime velazquez <jaimevelazquez@Admins-MBP.attlocal.net>
2020-07-23 22:50:03 -0500

    Navbar is now responsive, need to fix img

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -9,72 +9,33 @@ import Nav from 'react-bootstrap/Nav';
 
 function Navbar() {
   return (
-<>
-<nav className="navbar navbar-expand-md navbar-dark bg-dark text-light">
-    <a className="navbar-brand" 
-        onClick={() => {
-          alert("You are leaving website.")
-          }
-        }
-        href="https://www.bastropfoodpantry.org/">
-          <img
-            className="top_logo"
-            src={logo}
-            alt="Bastrop County Food Pantry Logo"
-        />
-      </a>
-  <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-    <span className="navbar-toggler-icon"></span>
-  </button>
-  <div className="collapse navbar-collapse navdiv" id="navbarNavAltMarkup">
-    <div className="navbar-nav">
-      <Link className="nav-link text-white" to="/recipes">
-              RECIPES
-      </Link>
-      <Link className="nav-link text-white" to="/shoppinglist">
-              SHOPPING LIST
-      </Link>
-      <a
-              className="nav-link text-white"
-              onClick={() => {
-                alert("You are leaving website.")
-              }
-              }
-              href="https://www.bastropfoodpantry.org/contact/"
-            >
-              CONTACT & LOCATION
-            </a>
-    </div>
-  </div>
-</nav>
 
-  <br />
-  <br />
-  <br />
-
-  <Navbar2 bg="dark" variant="dark">
+  <Navbar2 collapseOnSelect expand="lg" bg="dark" variant="dark">
     <Navbar2.Brand href="https://www.bastropfoodpantry.org/">
-    <img  onClick={() => {
-                alert("You are leaving website.")
-                }}
-              
-            className="top_logo"
-            src={logo}
-            alt="Bastrop County Food Pantry Logo"
-        />
-    </Navbar2.Brand>
-    <Nav className="mr-auto navdiv">
-      <Nav.Link href="/">RECIPES</Nav.Link>
-      <Nav.Link href="/shoppinglist">SHOPPING LIST</Nav.Link>
-      <Nav.Link href="https://www.bastropfoodpantry.org/contact/" 
-         onClick={() => {
-          alert("You are leaving website.")
-        }
-        }
-      >CONTACT & LOCATION</Nav.Link>
-    </Nav>
+      <img  onClick={() => {
+                  alert("You are leaving website.")
+                  }}
+                
+              className="top_logo"
+              src={logo}
+              alt="Bastrop County Food Pantry Logo"
+          />
+      </Navbar2.Brand>
+      <Navbar2.Toggle aria-controls="responsive-navbar-nav" />
+      <Navbar2.Collapse id="responsive-navbar-nav">
+        <Nav className="mr-auto navdiv">
+          <Nav.Link href="/">RECIPES</Nav.Link>
+          <Nav.Link href="/shoppinglist">SHOPPING LIST</Nav.Link>
+          <Nav.Link href="https://www.bastropfoodpantry.org/contact/" 
+            onClick={() => {
+              alert("You are leaving website.")
+            }
+            }
+          >CONTACT & LOCATION</Nav.Link>
+        </Nav>
+      </Navbar2.Collapse>
   </Navbar2>
-    </>
+
   );
 }
 
