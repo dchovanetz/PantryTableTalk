209cc33fb35b4a54fbbae59e24650352cb4bcb01
Monica Silva <msilva78617@gmail.com>
2020-07-29 19:19:19 -0500

    youtube component fixed

--- client/src/components/pages/recipe-result/YoutubeVideo.js
+++ client/src/components/pages/recipe-result/YoutubeVideo.js
@@ -4,27 +4,25 @@ import './YoutubeVideo.css'
 function YoutubeVideo(props) {
   //variable to check if youTUbe video is available--> for if/else statement
 const youTube = props.video
-useEffect(()=> {
-  console.log(youTube)
-}, [])
- 
-if (youTube) {
-  return (
-      <iframe 
-          width="600" 
-          height="375" 
-          src= {youTube}
-          frameBorder="0" 
-          allow="accelerometer; encrypted-media; gyroscope; picture-in-picture" 
-          allowFullScreen
-          title={`Video of ${props.name}`}>
-      </iframe>
-  )
-} else {
-  return (
-  <p className="p-noVideo">Cooking demonstration video coming soon!</p>
-  )
-};
-}
+const video = (
+  <iframe 
+  width="600" 
+  height="375" 
+  src= {youTube}
+  frameBorder="0" 
+  allow="accelerometer; encrypted-media; gyroscope; picture-in-picture" 
+  allowFullScreen
+  title={`Video of ${props.name}`}/>
+)
+
+const noVidMessage = (
+  <p className = "p-noVideo">Cooking demonstration video coming soon!</p>
+)
+return (
+  <div className="container" id="youtube">
+      {props.video ? video : noVidMessage}
+  </div>
+    );
+  }
 
 export default YoutubeVideo;
